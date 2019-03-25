d763c280d9f2c56b4bc40897132f8275ef46836d
MasterTyrr <SamRichards@users.noreply.github.com>
2019-03-25 15:03:40 -0400

    its broken....

--- RestaurantRATER/Controllers/RestaurantController.cs
+++ RestaurantRATER/Controllers/RestaurantController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RestaurantRATER.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,31 @@ namespace RestaurantRATER.Controllers
 {
     public class RestaurantController : Controller
     {
+        private RestaurantDBContext db = new RestaurantDBContext();
         // GET: Restaurant
         public ActionResult Index()
         {
+            return View(db.Restaurants.ToList());
+        }
+        //Get: Restaurant/create
+        public ActionResult Create()
+        {
+
             return View();
+        }
+
+        //Post: restaurant/create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Restaurant restaurant)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Restaurants.Add(restaurant);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(restaurant);
         }
     }
 }
